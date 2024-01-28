0841cedc8d8b3657162c741ee8ca5396b4e47a6c
Omar <omarashram2005@gmail.com>
2024-01-28 15:53:02 +0300

    animation

--- LZJOS-GameJam/Assets/Scripts/playercontroller.cs
+++ LZJOS-GameJam/Assets/Scripts/playercontroller.cs
@@ -5,36 +5,27 @@ using UnityEngine.Experimental.Rendering;
 
 public class playercontroller : MonoBehaviour {
     public float speed;
-    public float groundDist;
+   
 
-    public LayerMask terrainLayer;
+    
     public Rigidbody rb;
-    public SpriteRenderer sr;
+   
 
-    public Animator animator;
-
+    
     
 
     // Start is called before the first frame update
     void Start() {
         
         rb = gameObject.GetComponent<Rigidbody>();
-        animator = gameObject.GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
     void Update() {
-        RaycastHit hit;
-        Vector3 castPos = transform.position;
-        castPos.y += 1;
+       
 
-        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer)) {
-            if (hit.collider != null) {
-                Vector3 movePos = transform.position;
-                movePos.y = hit.point.y + groundDist;
-                transform.position = movePos;
-            }
-        }
+        
 
         //get input from the player
         float x = Input.GetAxis("Horizontal");
@@ -47,7 +38,7 @@ public class playercontroller : MonoBehaviour {
         {
             
             transform.rotation = Quaternion.Euler(0, -90, 0);
-            animator.SetBool("walking",true);
+           
         }
 
         if (Input.GetKeyDown(KeyCode.D))
